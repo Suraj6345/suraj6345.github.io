77a894c90b4a503c2b4c1bf978a7654e9e9b5b0e
Suraj6345 <suraj.m.pandit2009@gmail.com>
2026-01-05 22:05:40 +0530

    added for adsense code

---
+++ adsense.js
@@ -0,0 +1,7 @@
+(function () {
+  const s = document.createElement("script");
+  s.async = true;
+  s.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8890392660740083";
+  s.crossOrigin = "anonymous";
+  document.head.appendChild(s);
+})();
